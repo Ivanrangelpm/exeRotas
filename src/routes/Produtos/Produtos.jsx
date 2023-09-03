00d6ae3ef97d196f454e359bd9d79d8c6e8e3818
Ivanrangelpm <rm551005@fiap.com.br>
2023-09-03 08:44:51 -0300

    Rotas em editar produto

--- src/routes/Produtos/Produtos.jsx
+++ src/routes/Produtos/Produtos.jsx
@@ -6,15 +6,29 @@ export default function Produtos(){
     return(
         <main>
             <h1>Produtos</h1>
-            {
-                listaProdutos.map((prod) =>(
-                    <div key={prod.id}>
-                        <Link to={`/editar`}>
-
-                        </Link>
-                    </div>
-                ))}
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 
 }
+
+
+// return(
+//     <main>
+//         <h1>Produtos</h1>
+//         {
+//             listaProdutos.map((prod) =>(
+//                 <div key={prod.id}>
+//                     <Link to={`/editar`}>
+
+//                     </Link>
+//                 </div>
+//             ))}
+//     </main>
+// )
